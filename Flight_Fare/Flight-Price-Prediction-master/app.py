8d32780ce48ed7215736bc888adec9343d150893
Siddhiraj Ramesh Kolwankar <siddhirajk77@gmail.com>
2021-11-05 19:48:44 +0530

    updated app.py
    updated app.py

--- Flight_Fare/Flight-Price-Prediction-master/app.py
+++ Flight_Fare/Flight-Price-Prediction-master/app.py
@@ -12,6 +12,7 @@ from flask_mysqldb import MySQL
 
 
 
+
 # logging.basicConfig(level=logging.DEBUG, filename='test.log',
 #     format='%(asctime)s <:> %(levelname)s <:> %(message)s',
 #     datefmt='%Y-%m-%d <> %H:%M:%S ')
